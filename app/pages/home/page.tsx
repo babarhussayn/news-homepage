9dca88a71d940747fb5d123b81de27ee16fc165b
babarhussain <babarhusain262@gmail.com>
2024-10-29 17:07:31 +0500

    set

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const HomePage = () => {
   return (
     <section className="w-full  flex justify-center items-center container mx-auto">
-      <div className="w-[1200px] flex justify-center items-center ">
-        <div className="flex justify-between items-center  gap-4">
-          <div className="w-[1000px]">
+      <div className="w-[1200px] flex justify-center  gap-4">
+        <div className="flex justify-between  ">
+          <div className="w-[800px]">
             <div className="w-full">
               <Image
                 src="/images/image-web-3-desktop.jpg"
@@ -20,35 +20,35 @@ const HomePage = () => {
               <div>img</div>
             </div>
           </div>
-          <div className=" text-white ">
-            <div className="bg-black ">
-              <div className="flex justify-center items-center flex-col gap-8  ">
-                <div className="flex justify-start w-full p-8">
-                  <div>
-                    <h2 className="text-yellow-500 font-inter text-4xl">New</h2>
-
-                    <div>
-                      <h5>Hydrogen Vs electric cars</h5>
-                      <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
-                    </div>
-                    <hr />
-                  </div>
-                </div>
-
+        </div>
+        <div className=" text-white w-[400px] ">
+          <div className="bg-black h-[55vh]">
+            <div className="flex justify-center items-center flex-col gap-8  ">
+              <div className="flex justify-start w-full p-8">
                 <div>
+                  <h2 className="text-yellow-500 font-inter text-4xl">New</h2>
+
                   <div>
                     <h5>Hydrogen Vs electric cars</h5>
                     <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
                   </div>
                   <hr />
                 </div>
+              </div>
+
+              <div>
                 <div>
-                  <div>
-                    <h5>Hydrogen Vs electric cars</h5>
-                    <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
-                  </div>
-                  <hr />
+                  <h5>Hydrogen Vs electric cars</h5>
+                  <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
                 </div>
+                <hr />
+              </div>
+              <div>
+                <div>
+                  <h5>Hydrogen Vs electric cars</h5>
+                  <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
+                </div>
+                <hr />
               </div>
             </div>
           </div>
