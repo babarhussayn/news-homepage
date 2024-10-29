55164d48b7c96e140d11c44db08316189a26f009
babarhussain <babarhusain262@gmail.com>
2024-10-29 17:22:00 +0500

    side-bar setting

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -21,34 +21,41 @@ const HomePage = () => {
             </div>
           </div>
         </div>
-        <div className=" text-white w-[400px] ">
-          <div className="bg-black h-[55vh]">
+        <div className=" text-white w-[400px] bg-black">
+          <div className="bg-black h-[60vh]">
             <div className="flex justify-center items-center flex-col gap-8  ">
               <div className="flex justify-start w-full p-8">
-                <div>
+                <div className="">
                   <h2 className="text-yellow-500 font-inter text-4xl">New</h2>
 
                   <div>
                     <h5>Hydrogen Vs electric cars</h5>
-                    <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
+                    <p className="text-[#c2bebe]">
+                      Will hydrogen-fueled cars ever catch up to Evs?
+                    </p>
                   </div>
-                  <hr />
                 </div>
               </div>
+              <hr className="text-white w-full" />
 
-              <div>
-                <div>
+              <div className="p-4">
+                <div className="flex justify-start w-full flex-col">
                   <h5>Hydrogen Vs electric cars</h5>
-                  <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
+                  <p className="text-[#c2bebe]">
+                    What are the possible adverse effects of on-demand AI image
+                    generation?
+                  </p>
                 </div>
-                <hr />
               </div>
-              <div>
-                <div>
+              <hr className="text-white w-full" />
+              <div className="p-4">
+                <div className="flex justify-start flex-col w-full">
                   <h5>Hydrogen Vs electric cars</h5>
-                  <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
+                  <p className="text-[#c2bebe]">
+                    Private funding by VC firms is down 50% YOY. We take a look
+                    at what that means.
+                  </p>
                 </div>
-                <hr />
               </div>
             </div>
           </div>
