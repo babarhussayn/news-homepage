317601715b486a8da696c5fb53bd3f0b9a88eac4
babarhussain <babarhusain262@gmail.com>
2024-10-29 17:02:24 +0500

    side bar

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -1,7 +1,61 @@
+import Image from "next/image";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <section className="w-full  flex justify-center items-center container mx-auto">
+      <div className="w-[1200px] flex justify-center items-center ">
+        <div className="flex justify-between items-center  gap-4">
+          <div className="w-[1000px]">
+            <div className="w-full">
+              <Image
+                src="/images/image-web-3-desktop.jpg"
+                alt="desktop"
+                width={1200}
+                height={500}
+              />
+            </div>
+            <div className="flex justify-center items-center gap-8">
+              <div>con</div>
+              <div>img</div>
+            </div>
+          </div>
+          <div className=" text-white ">
+            <div className="bg-black ">
+              <div className="flex justify-center items-center flex-col gap-8  ">
+                <div className="flex justify-start w-full p-8">
+                  <div>
+                    <h2 className="text-yellow-500 font-inter text-4xl">New</h2>
+
+                    <div>
+                      <h5>Hydrogen Vs electric cars</h5>
+                      <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
+                    </div>
+                    <hr />
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <h5>Hydrogen Vs electric cars</h5>
+                    <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
+                  </div>
+                  <hr />
+                </div>
+                <div>
+                  <div>
+                    <h5>Hydrogen Vs electric cars</h5>
+                    <p>Will hydrogen-fueled cars ever catch up to Evs?</p>
+                  </div>
+                  <hr />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
